0f138cda9917a98c3487a966191e5d04b212d4b0
omunoz <steven.120189@gmail.com>
2021-04-16 12:07:29 -0400

    Added to timeouts-intervals.js

---
+++ timeouts-intervals.js
@@ -0,0 +1,19 @@
+function foodIsReady() {
+    console.log("The food is ready to be taken out of the oven");
+}
+
+setTimeout(foodIsReady, 5000);
+
+function getSwole() {
+    console.log("It's time for a push up");
+}
+
+setInterval(getSwole, 10000);
+
+function reminder(){
+    console.log("Its almost time for lunch")
+}
+function stopReminder(){
+    console.log("It's lunch time")
+}
+
